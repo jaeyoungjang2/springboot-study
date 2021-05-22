2ce31c98ceb5af469f20e1d3b3398697f69738d8
jaeyoungjang2 <lion85488@gmail.com>
2021-05-22 15:24:20 +0900

    add click me button

--- demo/src/main/java/com/example/demo/controller/mainController.java
+++ demo/src/main/java/com/example/demo/controller/mainController.java
@@ -2,13 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class mainController {
     @RequestMapping(value = "/")
     public String main() {
-        System.out.println("HI");
         return "main.html";
+    };
 
+    @RequestMapping("/get")
+    @ResponseBody
+    public String get() {
+        return "Hi";
     }
 }
