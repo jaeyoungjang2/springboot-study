29577a8a87740b871218b309a6b31ea6738781f1
jaeyoungjang2 <lion85488@gmail.com>
2021-05-23 10:32:46 +0900

    add thymeleaf

--- demo/src/main/java/com/example/demo/controller/mainController.java
+++ demo/src/main/java/com/example/demo/controller/mainController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -22,6 +24,12 @@ public class mainController {
     public String valueTest() {
         String value = "테스트 String";
         return value;
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("hello", "서버에서 보내준 값입니다.");
+        return "/hello.html";
     }
 
 }
